bb9c63d38578b21e29fbe2b99b36f86ab7cd2f5e
NGoyal18 <67914999+NGoyal18@users.noreply.github.com>
2025-04-02 20:43:50 -0400

    final frontend

--- client/src/components/main/games/guessGamePage/index.tsx
+++ client/src/components/main/games/guessGamePage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import { Box, Button, List, ListItem, TextField, Typography } from '@mui/material';
 import { GameInstance, GuessTheIngredientState } from '../../../../types/types';
 import useGuessTheIngredientPage from '../../../../hooks/useGuessTheIngredientPage';
 
@@ -34,83 +35,121 @@ const GuessTheIngredientPage = ({
     winner === gameInstance.state.player1
       ? gameInstance.state.attempts.player1
       : gameInstance.state.attempts.player2;
-
   return (
-    <>
-      <div className='guess-rules'>
-        <h2>Rules of Guess the Ingredient</h2>
-        <p>Try to guess the hidden ingredient before your opponent!</p>
-        <ol>
-          <li>The game has two players.</li>
-          <li>Players take turns guessing the hidden ingredient.</li>
-          <li>If you guess incorrectly, you will receive hints.</li>
-          <li>The first player to guess correctly wins!</li>
-        </ol>
-      </div>
-      {gameInstance.state.status === 'OVER' && (
-        <p>
-          <strong>Winner:</strong> {gameInstance.state.winners?.join(', ') || 'No winner'}
-          <br />
-          <strong>Score:</strong> {`${winnerScore}, ${winnerAttempts} attempt(s)`}
-        </p>
-      )}
-      <div className='guess-game-details'>
-        <h2>The Current Kitchen:</h2>
-        <p>
+    <Box p={3}>
+      <Typography variant='h5' sx={{ fontWeight: 'bold', color: '#FFA725' }} gutterBottom>
+        Rules:
+      </Typography>
+      <Typography variant='body1' paragraph>
+        The game of Guess the Ingredient is played as follows:
+      </Typography>
+      <ul>
+        <li>The game has two players.</li>
+        <li>Players take turns guessing the hidden ingredient.</li>
+        <li>If you guess incorrectly, you will receive hints.</li>
+        <li>The first player to guess correctly wins!</li>
+      </ul>
+      <Typography variant='body1' paragraph>
+        Try to guess the hidden ingredient before your opponent!
+      </Typography>
+
+      <Box mt={3}>
+        <Typography variant='h6' sx={{ fontWeight: 'bold', color: '#FFA725' }} gutterBottom>
+          Current Kitchen:
+        </Typography>
+        <Typography variant='body1'>
           <strong>Chef 1:</strong> {player1}
-        </p>
-        <p>
+        </Typography>
+        <Typography variant='body1'>
           <strong>Chef 2:</strong> {player2}
-        </p>
-        <p>
+        </Typography>
+        <Typography variant='body1'>
           <strong>Who&apos;s Cooking?:</strong> {gameInstance.state.currentPlayer}
-        </p>
-        <p>
+        </Typography>
+        <Typography variant='body1'>
           <strong>Your Attempts:</strong>{' '}
           {gameInstance.state.attempts[getPlayerKey(user.username)] || 0}
-        </p>
-        <div className='ingredient-image'>
-          <div className='ingredient-image'>
+        </Typography>
+
+        {gameInstance.state.status === 'OVER' && (
+          <Typography variant='body1'>
+            <strong>Winner:</strong> {gameInstance.state.winners?.join(', ') || 'No winner'}
+            <br />
+            <strong>Score:</strong> {`${winnerScore}, ${winnerAttempts} attempt(s)`}
+          </Typography>
+        )}
+
+        <Box mt={3}>
+          <Typography
+            variant='h6'
+            sx={{ mb: 3, fontWeight: 'bold', color: '#FFA725' }}
+            gutterBottom>
+            Ingredient to Guess:
+          </Typography>
+          <Box className='ingredient-image'>
             <img
               src={gameInstance.state.imageURL}
               alt='Guess the ingredient'
               style={{
                 filter: `blur(10px)`,
                 transition: 'filter 0.5s ease-in-out',
+                maxWidth: '100%',
               }}
             />
-          </div>
-        </div>
-        {hint && (
-          <p>
-            <strong>Hint:</strong> {hint}
-          </p>
-        )}
-        {gameInstance.state.status === 'IN_PROGRESS' && isCurrentPlayer && (
-          <div className='guess-game-input'>
-            <h3>Your Turn! Make a Guess</h3>
-            <input
-              type='text'
-              className='input-guess'
-              value={guess}
-              onChange={handleInputChange}
-              placeholder='Enter your guess'
-            />
-            <button className='btn-submit' onClick={handleMakeGuess}>
-              Submit Guess
-            </button>
-          </div>
-        )}
-        {!isCurrentPlayer && gameInstance.state.status === 'IN_PROGRESS' && (
-          <p>Waiting for {gameInstance.state.currentPlayer} to guess...</p>
-        )}
-        {gameInstance.state.status === 'OVER' && (
-          <p>
-            <strong>Correct Ingredient:</strong> {gameInstance.state.correctIngredient}
-          </p>
-        )}
-      </div>
-    </>
+          </Box>
+
+          {hint && (
+            <Typography variant='body1' sx={{ mt: 2 }}>
+              <strong>Hint:</strong> {hint}
+            </Typography>
+          )}
+
+          {gameInstance.state.status === 'IN_PROGRESS' && isCurrentPlayer && (
+            <Box mt={3}>
+              <Typography variant='h6' sx={{ fontWeight: 'bold', color: '#FFA725' }} gutterBottom>
+                Your Turn! Make a Guess.
+              </Typography>
+              <TextField
+                type='text'
+                value={guess}
+                onChange={handleInputChange}
+                fullWidth
+                sx={{
+                  'marginBottom': 2,
+                  '& .MuiOutlinedInput-root': {
+                    '&.Mui-focused fieldset': {
+                      borderColor: '#6A9C89', // Set custom border color on focus
+                    },
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: '#6A9C89', // Set custom label color on focus
+                  },
+                }}
+              />
+              <Button
+                variant='contained'
+                color='primary'
+                onClick={handleMakeGuess}
+                sx={{ backgroundColor: '#FFA725' }}>
+                Submit Guess
+              </Button>
+            </Box>
+          )}
+
+          {!isCurrentPlayer && gameInstance.state.status === 'IN_PROGRESS' && (
+            <Typography variant='body1' mt={2}>
+              Waiting for {gameInstance.state.currentPlayer} to guess...
+            </Typography>
+          )}
+
+          {gameInstance.state.status === 'OVER' && (
+            <Typography variant='body1' mt={2}>
+              <strong>Correct Ingredient:</strong> {gameInstance.state.correctIngredient}
+            </Typography>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
