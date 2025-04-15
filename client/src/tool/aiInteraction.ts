0d730e336d9e3b726975ed285c965928e1728e1c
Joshua McDonald <mcdonald.jos@northeastern.edu>
2025-04-15 09:25:43 -0400

    Update aiInteraction.ts

--- client/src/tool/aiInteraction.ts
+++ client/src/tool/aiInteraction.ts
@@ -1,35 +1,37 @@
 import axios from 'axios';
 
-// Constants for utilizing the Hugging Face AI model
-const MODEL_NAME = 'mistralai/Mistral-7B-Instruct-v0.3';
-
-/**
- * Calls the Hugging Face API to get a response for the given message.
- *
- * @param message - The user's message to send to Hugging Face
- * @returns - The AI-generated response.
- */
 const getAIResponse = async (message: string, apiKey: string): Promise<string> => {
-  const url = `https://api-inference.huggingface.co/models/${MODEL_NAME}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
   const headers = {
-    Authorization: `Bearer ${apiKey}`,
+    'Content-Type': 'application/json',
   };
 
   const systemPrompt = `You are a master chef and recipe creator, skilled in crafting delicious and creative recipes for any cuisine or dietary preference. 
-  Provide clear, step-by-step instructions, including precise ingredient measurements, cooking techniques, and helpful tips. Provide a recipe 
-  without asking follow-up questions if asked for a recipe or cooking advice in response to the question. Return your answer as properly formatted markdown.`;
+Provide clear, step-by-step instructions, including precise ingredient measurements, cooking techniques, and helpful tips. Provide a recipe 
+without asking follow-up questions if asked for a recipe or cooking advice in response to the question. Return your answer as properly formatted markdown.`;
 
   const separator = '\n**Start Recipe:**\n';
 
   const payload = {
-    inputs: `${systemPrompt + message + separator}`,
+    contents: [
+      {
+        parts: [
+          {
+            text: `${systemPrompt + message + separator}`,
+          },
+        ],
+      },
+    ],
   };
 
   try {
     const response = await axios.post(url, payload, { headers });
     const { data } = response;
-    return data[0].generated_text.split(separator)[1].trim();
+
+    const generatedText = data.candidates?.[0]?.content?.parts?.[0]?.text;
+
+		return generatedText
   } catch (err) {
     return 'The Munch Master could not understand your message :(';
   }
